3960fee3419b302280ec6930e4737bd72b8adba5
Ant00000ny <Ant00000ny@icloud.com>
2024-06-17 14:35:35 +0800

    add some validation

--- src/main/kotlin/fun/fantasea/tracemoe/api/SearchByImageUrl.kt
+++ src/main/kotlin/fun/fantasea/tracemoe/api/SearchByImageUrl.kt
@@ -1,24 +1,29 @@
 package `fun`.fantasea.tracemoe.api
 
 import `fun`.fantasea.tracemoe.TraceMoeClient
+import `fun`.fantasea.tracemoe.exception.ErrorException
 import `fun`.fantasea.tracemoe.model.TraceMoeResponse
 import `fun`.fantasea.tracemoe.utilility.Constant
 import `fun`.fantasea.tracemoe.utilility.Function.convertTo
-import okhttp3.HttpUrl
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.Request
 
 /**
  * curl "https://api.trace.moe/search?url=https://images.plurk.com/32B15UXxymfSMwKGTObY5e.jpg"
  */
 class SearchByImageUrl(
-    private val imageUrl: HttpUrl,
+    private val imageUrl: String,
     private val cutBorders: Boolean = false,
     private val anilistID: Int? = null,
 ) : TraceMoeApi<TraceMoeResponse> {
     override fun executeBy(client: TraceMoeClient): TraceMoeResponse {
+        if (imageUrl.toHttpUrlOrNull() == null) {
+            throw ErrorException("imageUrl is not a valid http url")
+        }
+
         val url = Constant.BASE_SEARCH_URL.newBuilder()
             .apply {
-                addQueryParameter("url", imageUrl.toString())
+                addQueryParameter("url", imageUrl)
                 if (cutBorders) addQueryParameter("cutBorders", null)
                 if (anilistID != null) addQueryParameter("anilistID", anilistID.toString())
             }
